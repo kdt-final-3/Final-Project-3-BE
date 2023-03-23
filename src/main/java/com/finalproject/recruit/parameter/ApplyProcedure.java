19d548300d4189a7d068167b6ce82dc1195d7d8f
Blooossom <taegyeong19@gmail.com>
2023-03-23 14:22:22 +0900

    feat : Enum 클래스 설정 - #15

---
+++ src/main/java/com/finalproject/recruit/parameter/ApplyProcedure.java
@@ -0,0 +1,12 @@
+package com.finalproject.recruit.parameter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ApplyProcedure{
+    서류제출,
+    면접,
+    최종조율
+}
---
+++ src/main/java/com/finalproject/recruit/parameter/MilitaryClass.java
@@ -0,0 +1,13 @@
+package com.finalproject.recruit.parameter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MilitaryClass{
+    병장,
+    상병,
+    일병,
+    이등병
+}
