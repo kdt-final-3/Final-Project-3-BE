81bfdf01d7668e841239fb9c6be5707c296a54ac
Blooossom <taegyeong19@gmail.com>
2023-03-24 15:22:44 +0900

    feat : Column 추가 - #15

--- src/main/java/com/finalproject/recruit/entity/Recruit.java
+++ src/main/java/com/finalproject/recruit/entity/Recruit.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Builder
@@ -23,6 +24,13 @@ public class Recruit {
     private Long recruitId;
 
     /**
+     * 기업연결 - 여러 채용공고가 하나의 기업과 연결되어 있을 수 있음
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    /**
      * 채용공고 명 ex)2022년 패캠 채용공고
      */
     @Column(name = "recruit_title")
@@ -35,16 +43,36 @@ public class Recruit {
     private String recruitContent;
 
     /**
-     * 채용 시작 날짜
+     * 서류 시작 날짜
      */
     @Column(name = "start_date")
     private LocalDateTime startDate;
 
     /**
-     * 채용 마감 일자
+     * 서류마감 일자
      */
     @Column(name = "end_date")
     private LocalDateTime endDate;
+
+    /**
+     * 면접 시작 날짜
+     */
+    @Column(name = "meet_start")
+    private LocalDateTime meetStart;
+
+    /**
+     * 면접 종료 날짜
+     */
+    @Column(name = "meet_end")
+    private LocalDateTime meetEnd;
+
+    /**
+     * 최종발표
+     */
+    @Column(name = "goal")
+    private LocalDateTime goal;
+
+
 
     /**
      * 삭제 여부
@@ -59,18 +87,16 @@ public class Recruit {
     private String recruitProcedure;
 
     /**
-     * 기업연결 - 여러 채용공고가 하나의 기업과 연결되어 있을 수 있음
-     */
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
-
-    /**
      * 기준 키워드
      */
     @Column(name = "keyword_standard")
     private String keywordStandard;
 
+    /**
+     * 자개소개서 주제
+     */
+    @Column(name = "resume_title")
+    private String resumeTitle;
 
 
 
