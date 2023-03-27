2319157527d061587630f08096e6dca8dedab100
Blooossom <taegyeong19@gmail.com>
2023-03-27 23:33:57 +0900

    feat : DTO 추가 - #18

--- src/main/java/com/finalproject/recruit/dto/member/MemberReqDTO.java
+++ src/main/java/com/finalproject/recruit/dto/member/MemberReqDTO.java
@@ -1,2 +1,32 @@
-package com.finalproject.recruit.dto.member;public class MemberReqDTO {
+package com.finalproject.recruit.dto.member;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MemberReqDTO {
+
+    /**
+     * 회원가입 요청 DTO
+     */
+    @Getter
+    @Setter
+    public static class SignUp {
+
+        private String memberEmail;
+
+        private String password;
+
+        private String memberPhone;
+
+        private String companyNum;
+
+        private String companyName;
+
+        private String ceoName;
+
+    }
 }
