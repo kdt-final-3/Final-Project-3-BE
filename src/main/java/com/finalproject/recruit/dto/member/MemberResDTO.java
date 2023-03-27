81e2cf4fe873d50768862591c08a330a901cbe0c
Blooossom <taegyeong19@gmail.com>
2023-03-27 23:43:50 +0900

    feat : Add Token Info in ResDTO - #19

--- src/main/java/com/finalproject/recruit/dto/member/MemberResDTO.java
+++ src/main/java/com/finalproject/recruit/dto/member/MemberResDTO.java
@@ -1,4 +1,25 @@
 package com.finalproject.recruit.dto.member;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class MemberResDTO {
+    
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    public static class TokenInfo {
+
+        private String grantType;
+
+        private String accessToken;
+
+        private String refreshToken;
+
+        private Long refreshTokenExpirationTime;
+    }
 }
