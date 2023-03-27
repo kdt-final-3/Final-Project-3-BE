3dd78cf8fe7db4a230da93a4c8cfb89ef6df61fa
Blooossom <taegyeong19@gmail.com>
2023-03-27 23:45:31 +0900

    feat : Add select method  - #19

--- src/main/java/com/finalproject/recruit/repository/MemberRepository.java
+++ src/main/java/com/finalproject/recruit/repository/MemberRepository.java
@@ -4,9 +4,12 @@ import com.finalproject.recruit.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
 
     boolean existsByMemberEmail(String email);
 
+    Optional<Member> findByMemberEmail(String email);
 }
