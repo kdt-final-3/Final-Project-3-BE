4dcde2e731c6459dd9add9b03be35344b3a252a0
Blooossom <taegyeong19@gmail.com>
2023-03-27 23:38:09 +0900

    #

--- src/main/java/com/finalproject/recruit/controller/AuthController.java
+++ src/main/java/com/finalproject/recruit/controller/AuthController.java
@@ -1,28 +1,12 @@
 package com.finalproject.recruit.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
-    /*===========================
-        회원가입
-     ===========================*/
 
 
-    /*===========================
-        아이디 중복체크
-     ===========================*/
-
-
-    /*===========================
-        로그인
-     ===========================*/
-
-
-    /*===========================
-        로그아웃
-     ===========================*/
 }
