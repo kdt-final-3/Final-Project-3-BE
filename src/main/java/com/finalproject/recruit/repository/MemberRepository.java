47b73f5aac45825198593be72739589c15ed97dd
Blooossom <taegyeong19@gmail.com>
2023-03-28 00:08:41 +0900

    feat : Add Query method - #21

--- src/main/java/com/finalproject/recruit/repository/MemberRepository.java
+++ src/main/java/com/finalproject/recruit/repository/MemberRepository.java
@@ -19,4 +19,8 @@ public interface MemberRepository extends JpaRepository<Member, String> {
     @Modifying
     @Query(value = "UPDATE member m SET m.password =: newPassowrd WHERE m.memberEmail", nativeQuery = true)
     Integer resetPassword(@Param("memberEmail") String email, @Param("newPassword") String newPassword);
+
+    @Modifying
+    @Query(value = "UPDATE member m SET m.password = :password, m.ceoName =: ceoName, m.memberPhone =:memberPhone WHERE m.memberEmail = :memberEmail", nativeQuery = true)
+    Integer updateInfo(@Param("password") String password, @Param("memberPhone") String memberPhone, @Param("ceoName") String ceoName, @Param("memberEmail") String memberEmail);
 }
