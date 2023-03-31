f2610ffc0d09779cb8b692b6786cae2bdbce5a87
Blooossom <taegyeong19@gmail.com>
2023-03-31 14:00:43 +0900

    feat : Entity 내 Update 메소드 추가 - #15

--- src/main/java/com/finalproject/recruit/entity/Member.java
+++ src/main/java/com/finalproject/recruit/entity/Member.java
@@ -1,14 +1,22 @@
 package com.finalproject.recruit.entity;
 
+import com.finalproject.recruit.dto.member.MemberReqDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Builder
@@ -62,6 +70,51 @@ public class Member {
     private boolean memberDelete;
 
 
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        List<String> roles = new ArrayList<>();
+//        roles.add("MEMBER");
+//        return roles.stream()
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toList());
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return memberEmail;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return false;
+//    }
 
+    public void resetPassword(String password) {
+        this.password = password;
+    }
 
+    public void updateMemberInfo(MemberReqDTO.Edit edit) {
+        this.password = edit.getPassword() == null || edit.getPassword().equals("")? password:edit.getPassword();
+        this.memberPhone = edit.getMemberPhone() == null || edit.getMemberPhone().equals("")? memberPhone: edit.getMemberPhone();
+        this.ceoName = edit.getCeoName() == null || edit.getCeoName().equals("")? ceoName:edit.getCeoName();
+    }
+
+    public void dropMember() {
+        this.memberDelete = true;
+    }
 }
