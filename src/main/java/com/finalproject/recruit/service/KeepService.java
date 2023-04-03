38aadd25544781885452ae5280ae136563cc6ec3
jisoooit <ydsn336@naver.com>
2023-04-03 15:13:33 +0900

    feat : 탈락인재 관련 api 페이징

--- src/main/java/com/finalproject/recruit/service/KeepService.java
+++ src/main/java/com/finalproject/recruit/service/KeepService.java
@@ -1,19 +1,18 @@
 package com.finalproject.recruit.service;
 
 import com.finalproject.recruit.dto.Response;
-import com.finalproject.recruit.entity.Apply;
-import com.finalproject.recruit.entity.Recruit;
-import com.finalproject.recruit.entity.Mail;
 import com.finalproject.recruit.dto.keep.ApplicantsRes;
+import com.finalproject.recruit.entity.Apply;
+import com.finalproject.recruit.entity.Mail;
+import com.finalproject.recruit.entity.Recruit;
 import com.finalproject.recruit.repository.ApplyRepository;
 import com.finalproject.recruit.repository.MailRepository;
 import com.finalproject.recruit.repository.RecruitRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,35 +25,33 @@ public class KeepService {
 
     private final Response response;
 
-    public ResponseEntity<?> recentRecruit(String email) {
+    public ResponseEntity<?> recentRecruit(String email, Pageable pageable) {
         Recruit recentRecruit = recruitRepository.findTopByAndMemberMemberEmailOrderByRecruitRegistedAt(email).orElse(null);
         if (recentRecruit == null) {
             return response.fail("채용공고가 존재하지 않습니다.");
         }
-        return dropApplicants(recentRecruit.getRecruitId());
+        return dropApplicants(recentRecruit.getRecruitId(), pageable);
     }
 
-    public ResponseEntity<?> dropApplicants(Long recruitId){
-        List<Apply> nonPassApplicants = applyRepository.findByRecruitRecruitIdAndFailApplyIsTrue(recruitId);
-        List<ApplicantsRes> applicantsResList = nonPassApplicants.stream()
+    public ResponseEntity<?> dropApplicants(Long recruitId, Pageable pageable){
+        Page<Apply> nonPassApplicants = applyRepository.findByRecruitRecruitIdAndFailApplyIsTrue(recruitId,pageable);
+        Page<ApplicantsRes> applicantsResList = nonPassApplicants
                 .map(apply -> ApplicantsRes.fromApply(
                         apply,
                         mailRepository.findTopByApplyApplyIdOrderByCreatedTimeDesc(apply.getApplyId())
                                 .map(Mail::getCreatedTime)
-                                .orElse(null)))
-                .collect(Collectors.toList());
+                                .orElse(null)));
         return response.success(applicantsResList);
     }
 
-    public ResponseEntity<?> eternalDeleteApplicants(Long recruitId) {
-        List<Apply> deletedApplicants = applyRepository.findByRecruitRecruitIdAndApplyDeleteIsTrue(recruitId);
-        List<ApplicantsRes> applicantsResList = deletedApplicants.stream()
+    public ResponseEntity<?> eternalDeleteApplicants(Long recruitId, Pageable pageable) {
+        Page<Apply> deletedApplicants = applyRepository.findByRecruitRecruitIdAndApplyDeleteIsTrue(recruitId, pageable);
+        Page<ApplicantsRes> applicantsResList = deletedApplicants
                 .map(apply -> ApplicantsRes.fromApply(
                         apply,
                         mailRepository.findTopByApplyApplyIdOrderByCreatedTimeDesc(apply.getApplyId())
                                 .map(Mail::getCreatedTime)
-                                .orElse(null)))
-                .collect(Collectors.toList());
+                                .orElse(null)));
         return response.success(applicantsResList);
     }
 }
