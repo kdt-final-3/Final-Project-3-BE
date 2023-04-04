44aaaea309c28f447760d2a57d8e7db2468d534c
crossbell8368 <godfather346@gmail.com>
2023-04-04 13:55:39 +0900

    refact: Jwt Related MemberDTO edit & MemberServcie edit * jwtProperties 도입으로 불필요해진 tokenDTO 'grantType' 제거 * RecruitService에서 Member 추출을 위해 사용되는 loadMemberByMemberEmail 메소드 추가

--- src/main/java/com/finalproject/recruit/dto/member/MemberResDTO.java
+++ src/main/java/com/finalproject/recruit/dto/member/MemberResDTO.java
@@ -2,22 +2,15 @@ package com.finalproject.recruit.dto.member;
 
 import lombok.*;
 
-@Getter
-@Setter
+@Getter @Setter
 @ToString
 public class MemberResDTO {
 
-    @Builder
-    @Getter
+    @Builder @Getter
     @AllArgsConstructor
     public static class TokenInfo {
-
-        private String grantType;
-
         private String accessToken;
-
         private String refreshToken;
-
         private Long refreshTokenExpirationTime;
     }
 }
